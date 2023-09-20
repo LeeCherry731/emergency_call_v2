4e94245f2916bf1c46dc58b029bfe519d776804d
LeeCherry731 <sangevol0@gmail.com>
2023-09-20 23:55:06 +0700

    fffff

---
+++ lib/config.dart
@@ -0,0 +1,3 @@
+class Config {
+  static String googleApiKey = "AIzaSyBG1czcWgot1iEkNTFkz2Bg_BAZueBvaiM";
+}
--- lib/pages/location.page/locaiton_detail.page.dart
+++ lib/pages/location.page/locaiton_detail.page.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'dart:math';
 
+import 'package:emergency_call_v2/config.dart';
 import 'package:emergency_call_v2/controllers/main.ctr.dart';
 import 'package:emergency_call_v2/models/location.doc.dart';
 import 'package:flutter/material.dart';
+import 'package:get_ip_address/get_ip_address.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class LocationDetailPage extends StatefulWidget {
   final LocationDoc location;
@@ -22,12 +25,14 @@ class _LocationDetailPageState extends State<LocationDetailPage> {
 
   final CameraPosition _kGooglePlex = const CameraPosition(
     target: LatLng(13.768566, 100.3425051),
-    zoom: 14.4746,
+    zoom: 13,
   );
 
   final location = Location();
+  LatLng currentLocation = const LatLng(0.0, 0.0);
 
   Set<Marker> markers = {};
+  List<LatLng> polylineCoordinates = [];
 
   Future<void> _goToYourLocation() async {
     var serviceEnabled = await location.serviceEnabled();
@@ -48,13 +53,45 @@ class _LocationDetailPageState extends State<LocationDetailPage> {
     }
     debugPrint(permissionGranted.toString());
 
-    var currentLocation = await location.getLocation();
+    final cur = await location.getLocation();
 
-    log.i(currentLocation);
+    currentLocation = LatLng(cur.latitude ?? 0, cur.longitude ?? 0);
 
     final CameraPosition myLocation = CameraPosition(
       target: LatLng(widget.location.latitude, widget.location.longitude),
-      zoom: 19.151926040649414,
+      zoom: 13,
+    );
+
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(myLocation));
+  }
+
+  Future<void> _goToMyLocation() async {
+    var serviceEnabled = await location.serviceEnabled();
+    debugPrint(serviceEnabled.toString());
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    var permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    debugPrint(permissionGranted.toString());
+
+    final cur = await location.getLocation();
+
+    currentLocation = LatLng(cur.latitude ?? 0, cur.longitude ?? 0);
+
+    final CameraPosition myLocation = CameraPosition(
+      target: LatLng(currentLocation.latitude, currentLocation.longitude),
+      zoom: 13,
     );
 
     final GoogleMapController controller = await _controller.future;
@@ -62,20 +99,71 @@ class _LocationDetailPageState extends State<LocationDetailPage> {
   }
 
   addMarker() async {
-    final id = Random().nextInt(10000);
     await Future.delayed(const Duration(milliseconds: 250));
 
     setState(() {
       final Marker marker = Marker(
-        markerId: MarkerId("$id"),
+        markerId: const MarkerId("sss"),
         position: LatLng(widget.location.latitude, widget.location.longitude),
-        infoWindow: const InfoWindow(title: "My Pin", snippet: '*'),
-        onTap: () {
-          log.i("$id Tap!");
-        },
+        infoWindow: const InfoWindow(title: "ที่อยู่เหตุด่วน", snippet: '*'),
+        onTap: () {},
       );
       markers = {marker};
     });
+  }
+
+  markMyLocation() async {
+    await Future.delayed(const Duration(milliseconds: 250));
+
+    setState(() {
+      final Marker marker = Marker(
+        markerId: const MarkerId("vvv"),
+        position: LatLng(currentLocation.latitude, currentLocation.longitude),
+        infoWindow: const InfoWindow(
+          title: "ที่อยู่ปัจจุบัน",
+          snippet: '*',
+        ),
+        onTap: () {},
+      );
+      markers = {marker, ...markers};
+    });
+  }
+
+  Future<void> goToPoint() async {
+    final PolylinePoints poly = PolylinePoints();
+
+    final from = PointLatLng(
+      currentLocation.latitude,
+      currentLocation.longitude,
+    );
+    final to = PointLatLng(
+      widget.location.latitude,
+      widget.location.longitude,
+    );
+
+    final PolylineResult result = await poly.getRouteBetweenCoordinates(
+        "AIzaSyBG1czcWgot1iEkNTFkz2Bg_BAZueBvaiM", from, to);
+
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    }
+    setState(() {});
+  }
+
+  Future<void> getIp() async {
+    try {
+      /// Initialize Ip Address
+      var ipAddress = IpAddress(type: RequestType.json);
+
+      /// Get the IpAddress based on requestType.
+      dynamic data = await ipAddress.getIpAddress();
+      print(data.toString());
+    } on IpAddressException catch (exception) {
+      /// Handle the exception.
+      print(exception.message);
+    }
   }
 
   @override
@@ -91,11 +179,47 @@ class _LocationDetailPageState extends State<LocationDetailPage> {
       appBar: AppBar(
         title: "ข้อมูล ${widget.location.name}".text.minFontSize(18).make(),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              getIp();
+            },
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.red,
+            onPressed: () {
+              markMyLocation();
+            },
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.amber,
+            onPressed: () {
+              _goToMyLocation();
+            },
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              goToPoint();
+            },
+          ),
+        ],
+      ),
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+        },
+        polylines: {
+          Polyline(
+            polylineId: PolylineId("route"),
+            points: polylineCoordinates,
+            color: Colors.blue,
+          )
         },
         markers: markers,
       ),
